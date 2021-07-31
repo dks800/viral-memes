1d8ef4d0f1f3f8889a46ebd574674125fbd4ee33
Dipak Sharma <desksharma@gmail.com>
2021-08-01 01:45:14 +0530

    Integrated API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Memes from './components/Memes';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [memes, setMemes] = useState([]);
+  const [allMemes, setAllMemes] = useState([]);
+  
+  function loadMoreMemes(){
+    let start = 0;
+    let end = memes.length + 20;
+    let newMemes = [...allMemes].splice(start, end);
+    setMemes(newMemes);
+    setTimeout(()=> {
+      window.scrollBy(0,800);
+    }, 1000);
+  }
+  
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => {
+      setAllMemes(data.data.memes);
+      let memeData = data.data.memes.slice(0, 20);
+      setMemes(memeData);
+    })
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="meme-page-title">Meme Collection</h1>
+      {
+        memes.length > 0 ?
+          <Memes data={memes} loadMore={loadMoreMemes}/>
+        :
+          <h1>Loading Memes... | Be ready!</h1>
+      }
+      
     </div>
   );
 }
---
+++ src/components/Memes.js
@@ -0,0 +1,25 @@
+import {React} from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+
+function Memes(props) {
+    let [memeData] = [props.data];
+    return (
+        <div className="meme-container">
+            {
+                memeData.map((meme) => (
+                    <Card key={meme.id}>
+                        <Card.Img variant="top" src={meme.url} />
+                        <Card.Body>
+                            <Card.Title>{ meme.name }</Card.Title>
+                            <Button variant="success" onClick={() => window.open(meme.url, "_blank")}>Download Meme</Button>
+                        </Card.Body>
+                    </Card>
+                ))
+            }
+            <Button className="loadMore" variant="danger" onClick={() => props.loadMore()}>Load More, It's Fun!</Button>
+        </div>
+    );
+}
+
+export default Memes;
